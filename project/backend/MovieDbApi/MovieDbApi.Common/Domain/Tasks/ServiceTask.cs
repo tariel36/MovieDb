5b60a2f62a6cd51b6a2fee32593ac90d167c94e5
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-06-02 18:53:25 +0200

    service task

---
+++ project/backend/MovieDbApi/MovieDbApi.Common/Domain/Tasks/ServiceTask.cs
@@ -0,0 +1,63 @@
+﻿using MovieDbApi.Common.Domain.Utility;
+
+namespace MovieDbApi.Common.Domain.Tasks
+{
+    public class ServiceTask
+        : IDisposable
+    {
+        public ServiceTask(DateTime startTime, TimeSpan delay, Action<CancellationToken> taskProcedure)
+        {
+            StartTime = startTime;
+            Delay = delay;
+            TaskProcedure = taskProcedure;
+
+            CancellationTokenSource = new CancellationTokenSource();
+            CancellationToken = CancellationTokenSource.Token;
+
+            Task = Task.Factory.StartNew(ServiceProcedure, CancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Current);
+        }
+
+        private Task Task { get; set; }
+
+        private CancellationTokenSource CancellationTokenSource { get; }
+        
+        private CancellationToken CancellationToken { get; }
+
+        private DateTime StartTime { get; }
+        
+        private TimeSpan Delay { get; }
+
+        private Action<CancellationToken> TaskProcedure { get; }
+
+        public void Dispose()
+        {
+            Extensions.SafeDispose(CancellationTokenSource);
+        }
+
+        public void Stop()
+        {
+            CancellationTokenSource.Cancel();
+        }
+
+        private async Task ServiceProcedure()
+        {
+            TimeSpan firstRunDelay = DateTime.UtcNow - StartTime;
+
+            await Task.Delay(firstRunDelay, CancellationToken);
+
+            while (!CancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    TaskProcedure(CancellationToken);
+
+                    await Task.Delay(Delay, CancellationToken);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
