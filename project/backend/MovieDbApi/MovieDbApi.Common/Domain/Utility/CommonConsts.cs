6221060264fd1b94c0d643a935031452ded906bb
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-06-02 18:53:30 +0200

    common consts

---
+++ project/backend/MovieDbApi/MovieDbApi.Common/Domain/Utility/CommonConsts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDbApi.Common.Domain.Utility
+{
+    public static class CommonConsts
+    {
+        public const string BaseLanguage = "en";
+    }
+}
