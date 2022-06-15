b9b66c5117beb4d3909900e3c041f5ee64e3f87b
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-06-15 18:21:02 +0200

    tools fix

--- project/backend/MovieDbApi/MovieDbApi.Tools/Program.cs
+++ project/backend/MovieDbApi/MovieDbApi.Tools/Program.cs
@@ -1,15 +1,11 @@
-﻿using System.Configuration;
-using System.Reflection;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using MovieDbApi.Common.Data.Specific;
-using MovieDbApi.Common.Domain.Apis.Specific.Anilist;
 using MovieDbApi.Common.Domain.Compression.Abstract;
 using MovieDbApi.Common.Domain.Compression.Specific;
-using MovieDbApi.Common.Domain.Crawling.Models;
-using MovieDbApi.Common.Domain.Crawling.Services;
-using MovieDbApi.Common.Domain.Media.Models.Data;
 using MovieDbApi.Common.Domain.Media.Services.Abstract;
 using MovieDbApi.Common.Domain.Media.Services.Specific;
+using MovieDbApi.Common.Maintenance.Logging.Abstract;
+using MovieDbApi.Common.Maintenance.Logging.Specific;
 
 AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -24,32 +20,13 @@ IConfigurationRoot configuration = new ConfigurationBuilder()
 
 IHashProvider hashProvider = new Md5HashProvider();
 
-MediaContext ctx = new MediaContext(configuration);
+ILoggerService logger = new LoggerService(new ILoggerSink[] { new ConsoleLoggerSink() });
 
-//List<TranslationCache> translations = ctx.TranslationCache.ToList();
-//foreach (var translation in translations)
-//{
-//    if (string.IsNullOrWhiteSpace(translation.SourceHash))
-//    {
-//        translation.SourceHash = hashProvider.Get(translation.Source);
-//        ctx.Update(translation);
-//        ctx.SaveChanges();
-//    }
-//}
-
-//MediaCrawlerService crawler = new MediaCrawlerService();
-//crawler.Crawl(new MediaCrawlContext()
-//{
-//    Path = @"\\Nuta-NAS\nutka\s\media\cartoons_pl"
-//});
+MediaContext ctx = new MediaContext(configuration, logger);
 
 IMediaService mediaService = new MediaService(ctx, hashProvider);
 
-MediaMonitor monitor = new MediaMonitor(configuration, mediaService);
+MediaMonitor monitor = new MediaMonitor(logger, configuration, mediaService);
 monitor.Work();
-
-
-//var api = new AniListDataProvider(string.Empty);
-//var res = api.GetByUrl("https://anilist.co/anime/113425/Kaifuku-Jutsushi-no-Yarinaoshi/");
 
 Console.WriteLine("Done");
