d165bfc2f41b0b56f1b404804e6bcf0da5e6c48b
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-06-16 17:39:59 +0200

    added base path

---
+++ project/backend/MovieDbApi/MovieDbApi.Common/Migrations/20220616153711_BaseScanPath.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieDbApi.Common.Migrations
+{
+    public partial class BaseScanPath : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT IGNORE INTO `ScannedPaths` (`Path`) SELECT '/app/ext_root' FROM DUAL WHERE NOT EXISTS (SELECT * FROM `ScannedPaths` WHERE `Path` = '/app/ext_root' LIMIT 1);");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
