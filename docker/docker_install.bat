3c37dd847379561344e3754386b3b016cedc7376
tariel36 <2964246+tariel36@users.noreply.github.com>
2022-06-16 16:39:48 +0200

    docker improvements

--- docker/docker_install.bat
+++ docker/docker_install.bat
@@ -1,4 +1,4 @@
-docker compose --env-file ./env up -d
+docker compose --env-file ./.env up -d
 ping -n 15 127.0.0.1 > nul
 docker exec -i moviedb-mysql mysql -uroot -proot -e "update mysql.user set host = '%' where user='root';"
 ping -n 15 127.0.0.1 > nul
